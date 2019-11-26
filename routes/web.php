87d767e25ea6041c8672f3509016abedd9a5fc6d
Mbahmo <anthonyleembahmo@gmail.com>
2019-11-26 19:15:24 +0800

    crud intiate

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,10 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resources([
+    'products' => 'ProductsController',
+    'orders'   => 'OrdersController',
+    'payments' => 'PaymentsController'
+]);
+
