d5a489b1d8cbe16f4b838d6e51a1188f1b9edad6
Mbahmo <anthonyleembahmo@gmail.com>
2019-12-01 20:52:14 +0800

    upload foto

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
+use \Validator;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -21,8 +24,26 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
-    {
-        return view('home');
+    public function index(){
+      $user = User::find(Auth::user()->id);
+    return view('home', compact('user'));
+    }
+    public function upload(Request $request){
+        $validator = Validator::make($request->all(), [
+            'gambar' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        if($validator->passes()) {
+            $input = $request->all();
+            $input['gambar'] = time().'.'.$request->gambar->getClientOriginalExtension();
+            $request->gambar->move(public_path('images'), $input['gambar']);
+            $user = User::find($input['id']);
+            $imagelama = (public_path('images').'/'.$user->image);
+            // dd($imagelama);
+            unlink($imagelama);
+            $user->image  = $input['gambar'];
+            $user->save();
+        }
+        return redirect('/home');
     }
 }
